cc681eebce08ea233c035f7c428676d176a290c1
gramdel <legovec96@gmail.com>
2022-04-16 18:27:05 +0300

    Почти готов метод трапеции

--- src/main/java/math/TrapezoidalRule.java
+++ src/main/java/math/TrapezoidalRule.java
@@ -4,6 +4,24 @@ import org.mariuszgromada.math.mxparser.Function;
 
 public class TrapezoidalRule {
     public static double calcIntegral(double a, double b, Function f, double eps) {
-        return 0;
+        double currResult = 0, prevResult = 0;
+        for (int n = 1; n <= 2 || Math.abs(currResult - prevResult) > eps; n *= 2) {
+            prevResult = currResult;
+            currResult = 0;
+            double h = (b - a) / n;
+            for (int i = 0; i <= n; i++) {
+                double tmp = f.calculate(a + h * i);
+                if (!Double.isFinite(tmp)) {
+                    //solve shit
+                }
+                if (i == 0 || i == n) {
+                    tmp /= 2;
+                }
+                currResult += tmp;
+            }
+            currResult *= h;
+            System.out.println("n = " + n + ", result = " + currResult);
+        }
+        return currResult;
     }
 }
